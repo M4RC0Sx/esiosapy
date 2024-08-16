93601bc2f9c2133567944db765f5cb05c38d27e3
M4RC0Sx <marcos3as3@gmail.com>
2024-08-17 00:26:11 +0200

    feat(client): core esiosapy client

---
+++ esiosapy/client.py
@@ -0,0 +1,26 @@
+from typing import Dict
+from urllib.parse import urljoin, urlparse
+
+import requests
+
+from esiosapy.managers.archive_manager import ArchiveManager
+from esiosapy.utils.request_helper import RequestHelper
+
+ESIOS_API_URL = "https://api.esios.ree.es/"
+
+
+class ESIOSAPYClient:
+    def __init__(self, token: str, base_url: str = ESIOS_API_URL):
+        self.token = token
+        self.base_url = base_url
+        self.request_helper = RequestHelper(base_url, token)
+
+        self.archives: ArchiveManager = ArchiveManager(self.request_helper)
+
+    def raw_request(self, url: str, headers: Dict[str, str] = {}) -> requests.Response:
+        headers = self.request_helper.add_default_headers(headers)
+
+        if urlparse(url).netloc == "":
+            url = urljoin(self.base_url, url)
+
+        return requests.get(url, headers={"Accept": "application/json"})
