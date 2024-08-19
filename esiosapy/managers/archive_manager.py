3634c0b8df19567894b7d8561b4618c8139ee46f
M4RC0Sx <marcos3as3@gmail.com>
2024-08-19 23:06:28 +0200

    docs(archive): add docstrings to archive manager

--- esiosapy/managers/archive_manager.py
+++ esiosapy/managers/archive_manager.py
@@ -7,13 +7,43 @@ from esiosapy.utils.request_helper import RequestHelper
 
 
 class ArchiveManager:
+    """
+    Manages archive-related operations for the ESIOS API.
+
+    This class provides methods to retrieve and filter archives from the ESIOS
+    API, such as listing all archives or filtering them by date or date range.
+    """
+
     def __init__(self, request_helper: RequestHelper) -> None:
+        """
+        Initializes the ArchiveManager with a RequestHelper.
+
+        :param request_helper: An instance of RequestHelper used to make API requests.
+        :type request_helper: RequestHelper
+        """
         self.request_helper = request_helper
 
     def _init_archive(self, archive: Dict[str, Union[str, int]]) -> Archive:
+        """
+        Initializes an Archive object from a dictionary of archive data.
+
+        :param archive: A dictionary containing archive data.
+        :type archive: Dict[str, Union[str, int]]
+        :return: An Archive object initialized with the provided data.
+        :rtype: Archive
+        """
         return Archive(**archive, raw=archive, _request_helper=self.request_helper)
 
     def list_all(self) -> List[Archive]:
+        """
+        Retrieves a list of all archives.
+
+        This method sends a GET request to the `/archives` endpoint and
+        returns a list of Archive objects representing all available archives.
+
+        :return: A list of Archive objects representing all archives.
+        :rtype: List[Archive]
+        """
         response = self.request_helper.get_request("/archives")
         return [self._init_archive(archive) for archive in response.json()["archives"]]
 
@@ -23,6 +53,24 @@ class ArchiveManager:
         date_type: Optional[ArchiveDateType] = None,
         taxonomy_terms: Optional[List[str]] = None,
     ) -> List[Archive]:
+        """
+        Retrieves a list of archives filtered by a specific date.
+
+        This method sends a GET request to the `/archives` endpoint with filters
+        based on the provided date, date type, and optional taxonomy terms.
+
+        :param target_dt: The target date for filtering archives. Can be a datetime
+                          object or an ISO 8601 formatted string.
+        :type target_dt: Union[datetime, str]
+        :param date_type: The type of date to filter by (e.g., publication date),
+                          defaults to None.
+        :type date_type: Optional[ArchiveDateType], optional
+        :param taxonomy_terms: A list of taxonomy terms to further filter the archives,
+                               defaults to None.
+        :type taxonomy_terms: Optional[List[str]], optional
+        :return: A list of Archive objects filtered by the specified date.
+        :rtype: List[Archive]
+        """
         if isinstance(target_dt, datetime):
             target_dt = target_dt.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
 
@@ -42,6 +90,28 @@ class ArchiveManager:
         date_type: Optional[ArchiveDateType] = None,
         taxonomy_terms: Optional[List[str]] = None,
     ) -> List[Archive]:
+        """
+        Retrieves a list of archives filtered by a date range.
+
+        This method sends a GET request to the `/archives` endpoint with filters
+        based on the provided start and end dates, date type, and optional taxonomy
+        terms.
+
+        :param target_dt_start: The start date for filtering archives. Can be a datetime
+                                object or an ISO 8601 formatted string.
+        :type target_dt_start: Union[datetime, str]
+        :param target_dt_end: The end date for filtering archives. Can be a datetime
+                              object or an ISO 8601 formatted string.
+        :type target_dt_end: Union[datetime, str]
+        :param date_type: The type of date to filter by (e.g., publication date),
+                          defaults to None.
+        :type date_type: Optional[ArchiveDateType], optional
+        :param taxonomy_terms: A list of taxonomy terms to further filter the archives,
+                               defaults to None.
+        :type taxonomy_terms: Optional[List[str]], optional
+        :return: A list of Archive objects filtered by the specified date range.
+        :rtype: List[Archive]
+        """
         if isinstance(target_dt_start, datetime):
             target_dt_start = target_dt_start.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
         if isinstance(target_dt_end, datetime):
