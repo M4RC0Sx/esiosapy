8a5b8b0df0407a4e3322aa0d502d4de5f8dade0d
M4RC0Sx <marcos3as3@gmail.com>
2024-08-19 22:59:05 +0200

    docs(utils): add docstrings to utils module

--- esiosapy/utils/request_helper.py
+++ esiosapy/utils/request_helper.py
@@ -5,11 +5,38 @@ import requests
 
 
 class RequestHelper:
+    """
+    A helper class to manage HTTP requests, including adding default headers
+    and making GET requests.
+
+    This class simplifies the process of making HTTP GET requests by handling
+    common tasks such as setting default headers and constructing the full URL.
+    """
+
     def __init__(self, base_url: str, token: str):
+        """
+        Initializes the RequestHelper with a base URL and an API token.
+
+        :param base_url: The base URL for the API endpoints.
+        :type base_url: str
+        :param token: The API token used for authentication in requests.
+        :type token: str
+        """
         self.base_url = base_url
         self.token = token
 
     def add_default_headers(self, headers: Dict[str, str]) -> Dict[str, str]:
+        """
+        Adds default headers to the provided headers dictionary.
+
+        This method adds the 'Accept', 'Content-Type', and 'x-api-key' headers
+        to the headers dictionary if they are not already present.
+
+        :param headers: The headers to which the defaults will be added.
+        :type headers: Dict[str, str]
+        :return: The updated headers dictionary with default headers added.
+        :rtype: Dict[str, str]
+        """
         if "Accept" not in headers:
             headers["Accept"] = "application/json; application/vnd.esios-api-v1+json"
 
@@ -27,6 +54,24 @@ class RequestHelper:
         headers: Optional[Dict[str, str]] = None,
         params: Optional[Dict[str, Union[str, int, List[str]]]] = None,
     ) -> requests.Response:
+        """
+        Makes a GET request to the specified path, with optional headers and parameters.
+
+        This method constructs the full URL by combining the base URL and the
+        provided path. It then sends a GET request to this URL, including any
+        provided headers and query parameters.
+
+        :param path: The endpoint path to be appended to the base URL.
+        :type path: str
+        :param headers: Optional headers to include in the request. If not provided,
+                        default headers will be added. Defaults to None.
+        :type headers: Optional[Dict[str, str]], optional
+        :param params: Optional query parameters to include in the request. This can
+                       include strings, integers, or lists of strings. Defaults to None.
+        :type params: Optional[Dict[str, Union[str, int, List[str]]]], optional
+        :return: The response object resulting from the GET request.
+        :rtype: requests.Response
+        """
         if headers is None:
             headers = {}
         if params is None:
--- esiosapy/utils/zip_utils.py
+++ esiosapy/utils/zip_utils.py
@@ -6,6 +6,27 @@ from zipfile import ZipFile
 def recursive_unzip(
     zip_path: Union[str, Path], unzip_path: Union[str, Path], remove: bool = False
 ) -> None:
+    """
+    Recursively unzips a ZIP file and all nested ZIP files
+    within it to a specified directory.
+
+    This function extracts the contents of the provided ZIP file to the specified
+    directory. If any ZIP files are found within the extracted contents, it will
+    recursively extract them into their respective directories. Optionally, it can
+    also delete the original ZIP files after extraction.
+
+    :param zip_path: The path to the ZIP file to be unzipped.
+                     This can be a string or a Path object.
+    :type zip_path: Union[str, Path]
+    :param unzip_path: The directory where the contents of the ZIP file
+                       should be extracted. This can be a string or a Path object.
+    :type unzip_path: Union[str, Path]
+    :param remove: If set to True, the original ZIP files will be deleted
+                   after extraction, including nested ZIP files. Defaults to False.
+    :type remove: bool, optional
+
+    :return: None
+    """
     zip_path, unzip_path = Path(zip_path), Path(unzip_path)
 
     with ZipFile(zip_path, "r") as zip_ref:
