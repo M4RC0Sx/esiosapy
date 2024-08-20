3ce4cae1bee899259be314d79b08940941f9bd7d
M4RC0Sx <marcos3as3@gmail.com>
2024-08-20 15:05:47 +0200

    docs(archive): add docstrings to archive models

--- esiosapy/models/archive/archive_download.py
+++ esiosapy/models/archive/archive_download.py
@@ -3,5 +3,22 @@ from dataclasses import dataclass
 
 @dataclass
 class ArchiveDownload:
+    """
+    Represents the download information for an archive.
+
+    This dataclass contains information about the downloadable content associated
+    with an archive, including
+    the name of the file and the URL where it can be accessed.
+    """
+
     name: str
+    """The name of the file to be downloaded.
+
+    :type: str
+    """
+
     url: str
+    """The URL where the file can be downloaded.
+
+    :type: str
+    """
